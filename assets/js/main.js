1dfd13a5dc7f2c4f0b7b5fefb6ff8694e6a35e87
bedimcode <61248995+bedimcode@users.noreply.github.com>
2020-05-09 08:48:32 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// SHOW MENU
+const showMenu = (toggleId, navbarId,bodyId) =>{
+    const toggle = document.getElementById(toggleId),
+    navbar = document.getElementById(navbarId),
+    bodypadding = document.getElementById(bodyId)
+
+    if(toggle && navbar){
+        toggle.addEventListener('click', ()=>{
+            // APARECER MENU
+            navbar.classList.toggle('show')
+            // ROTATE TOGGLE
+            toggle.classList.toggle('rotate')
+            // PADDING BODY
+            bodypadding.classList.toggle('expander')
+        })
+    }
+}
+showMenu('nav-toggle','navbar','body')
+
+// LINK ACTIVE COLOR
+const linkColor = document.querySelectorAll('.nav__link');   
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active'));
+    this.classList.add('active');
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink));
+
